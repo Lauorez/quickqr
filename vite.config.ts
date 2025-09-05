17cb093ba00e912ce346fe6f27671ba4c5a561fb
Lauorez <l.grove@bau-block.com>
2025-09-05 10:49:57 +0200

    ????

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",  // for GitHub Pages
+  base: "/quickqr/",  // for GitHub Pages
 })
